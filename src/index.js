4c814405b114b3231b0f303b99adc107b9aff3a4
Yura <tt@list.ru>
2020-12-08 13:37:31 +0200

    added stringify function to return

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-// import fs from 'fs';
 import _ from 'lodash';
 import parseFile from './read.js';
+import stringify from './output.js';
 
 export default (path1, path2) => {
   const file1 = parseFile(path1);
   const file2 = parseFile(path2);
-  const statusSymbols = { removed: '-', added: '+', unchanged: ' ' };
   const sortedUniqKeys = _.union(Object.keys(file1), Object.keys(file2)).sort();
   const elementsWithStatus = sortedUniqKeys.reduce((acc, key) => {
-    const valueFile1 = file1[key];
-    const valueFile2 = file2[key];
-    const removed = `  ${statusSymbols.removed} ${key}: ${valueFile1}`;
-    const added = `  ${statusSymbols.added} ${key}: ${valueFile2}`;
-    const unchanged = `  ${statusSymbols.unchanged} ${key}: ${valueFile1}`;
+    const newKey1 = `${key}: ${file1[key]}`;
+    const newKey2 = `${key}: ${file2[key]}`;
     if (_.has(file1, key) && _.has(file2, key)) {
-      if (valueFile1 === valueFile2) {
-        return [...acc, unchanged];
+      if (file1[key] === file2[key]) {
+        acc[newKey1] = 'unchanged';
+      } else {
+        acc[newKey1] = 'removed';
+        acc[newKey2] = 'added';
       }
-      return [...acc, removed, added];
-    } if (_.has(file1, key) && !_.has(file2, key)) {
-      return [...acc, removed];
+    } else if (_.has(file1, key) && !_.has(file2, key)) {
+      acc[newKey1] = 'removed';
+    } else {
+      acc[newKey2] = 'added';
     }
-    return [...acc, added];
-  }, []);
-  const result = elementsWithStatus.join('\n');
-  return ['{', result, '}'].join('\n');
+    return acc;
+  }, {});
+  return stringify(elementsWithStatus);
 };
