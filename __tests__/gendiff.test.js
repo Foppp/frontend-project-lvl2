63e9b5fb7507635ffd38bd8cfc69dfd4a5ecb087
Yura <tt@list.ru>
2020-12-10 16:14:49 +0200

    Renamed filenames in fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,16 +12,16 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 describe('Difference between two files', () => {
   test('.json files', () => {
-    const result = readFile('result1.txt');
-    const beforeFile1 = getFixturePath('before1.json');
-    const afterFile1 = getFixturePath('after1.json');
+    const result = readFile('jsonResultAB.txt');
+    const beforeFile1 = getFixturePath('jsonFileA.json');
+    const afterFile1 = getFixturePath('jsonFileB.json');
     expect(genDiff(beforeFile1, afterFile1)).toEqual(result);
   });
 
   test('.yml files', () => {
-    const result2 = readFile('yamlResult1.txt');
-    const beforeFile11 = getFixturePath('yamlFile1.yml');
-    const afterFile22 = getFixturePath('yamlFile2.yml');
+    const result2 = readFile('yamlResultAB.txt');
+    const beforeFile11 = getFixturePath('yamlFileA.yml');
+    const afterFile22 = getFixturePath('yamlFileB.yml');
     expect(genDiff(beforeFile11, afterFile22)).toEqual(result2);
   });
 });
