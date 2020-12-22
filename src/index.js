8a46d9421b4f968f9b5020e8a69a8ac33da7db5a
Yura <tt@list.ru>
2020-12-22 21:45:54 +0200

    renamed formater to stylish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import parseFile from './parser.js';
 import makeDiff from './diff.js';
-import print from './print.js';
+import stylish from './print.js';
 
 export default (path1, path2) => {
   const parsedFile1 = parseFile(path1);
   const parsedFile2 = parseFile(path2);
   const result = makeDiff(parsedFile1, parsedFile2);
-  return print(result);
+  return stylish(result);
 };
