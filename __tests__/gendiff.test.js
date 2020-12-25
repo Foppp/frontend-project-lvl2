e4280a1653ffa60299ea0a1c38276eb571de878b
Yura <tt@list.ru>
2020-12-25 17:20:05 +0200

    added test for plain result

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,18 +10,31 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('Difference between two files', () => {
-  test('Compare .json files', () => {
-    const result = readFile('nestedJsonResult.txt');
+describe('Difference between two .json files', () => {
+  test('Compare stylish format', () => {
+    const result = readFile('stylishResult.txt');
     const beforeFile1 = getFixturePath('nestedJsonFile1.json');
     const afterFile1 = getFixturePath('nestedJsonFile2.json');
     expect(genDiff(beforeFile1, afterFile1)).toEqual(result);
   });
-
-  test('Compare .yml files', () => {
-    const result2 = readFile('yamlResultAB.txt');
+  test('Compare plain format', () => {
+    const result = readFile('plainResult.txt');
+    const beforeFile1 = getFixturePath('nestedJsonFile1.json');
+    const afterFile1 = getFixturePath('nestedJsonFile2.json');
+    expect(genDiff(beforeFile1, afterFile1, 'plain')).toEqual(result);
+  });
+});
+describe('Difference between two .yaml files', () => {
+  test('Compare stylish format', () => {
+    const result2 = readFile('stylishResult.txt');
     const beforeFile11 = getFixturePath('yamlFileA.yml');
     const afterFile22 = getFixturePath('yamlFileB.yml');
     expect(genDiff(beforeFile11, afterFile22)).toEqual(result2);
+  });
+  test('Compare plain format', () => {
+    const result2 = readFile('plainResult.txt');
+    const beforeFile11 = getFixturePath('yamlFileA.yml');
+    const afterFile22 = getFixturePath('yamlFileB.yml');
+    expect(genDiff(beforeFile11, afterFile22, 'plain')).toEqual(result2);
   });
 });
