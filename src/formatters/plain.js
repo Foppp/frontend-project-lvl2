053b505636fd3b0fca6703a8acc5b1fe75284228
Yura <tt@list.ru>
2020-12-25 21:43:35 +0200

    modified file

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,20 +1,19 @@
-import _ from 'lodash';
 import { normalize } from '../utils.js';
 
+const output = {
+  removed: (p) => `Property '${p}' was removed`,
+  added: (p, val) => `Property '${p}' was added with value: ${normalize(val)}`,
+  updated: (p, val, nVal) => `Property '${p}' was updated. From ${normalize(val)} to ${normalize(nVal)}`,
+  nested: () => [],
+};
 const plain = (elements, propPath = []) => {
-  const output = {
-    removed: (p) => `Property '${p}' was removed`,
-    added: (p, val) => `Property '${p}' was added with value: ${normalize(val)}`,
-    updated: (p, val, nVal) => `Property '${p}' was updated. From ${normalize(val)} to ${normalize(nVal)}`,
-    nested: () => [],
-  };
   const result = elements
     .filter((el) => el.status !== 'unchanged')
     .flatMap((element) => {
       const { name, value, status } = element;
       const propAcc = [...propPath, name];
-      if (!_.has(element, 'children')) {
-        if (_.has(element, 'newValue')) {
+      if (status !== 'nested') {
+        if (status === 'updated') {
           const [removed, added] = element.newValue;
           return output[status](propAcc.join('.'), removed.value, added.value);
         }
