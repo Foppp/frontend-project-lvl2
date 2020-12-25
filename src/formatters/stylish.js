98b380f2ec522b0a00f98952c4bf3f912918718d
Yura <tt@list.ru>
2020-12-25 21:16:13 +0200

    modified stylish file

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,28 +1,26 @@
 import _ from 'lodash';
 
-const stylish = (astTree, replacer = ' ', spacesCount = 2) => {
-  const symbols = {
-    removed: '-', added: '+', unchanged: ' ', nested: ' ',
-  };
-  const iter = (tree, depth) => {
-    if (!_.isObject(...tree)) {
-      return tree;
+const stylish = (tree, depth = 1) => {
+  const symbols = { removed: '-', added: '+' };
+  if (!_.isObject(...tree)) {
+    return tree;
+  }
+  const replacer = ' ';
+  const spacesCount = 2;
+  const spaceSize = depth * spacesCount;
+  const spaceInd = replacer.repeat(spaceSize);
+  const spaceBr = replacer.repeat(spaceSize - spacesCount);
+  const result = tree.flatMap((element) => {
+    const { name, value, status } = element;
+    const currentValue = (_.has(element, 'children')) ? element.children : value;
+    const currentSymbol = symbols[status] ?? replacer;
+    if (_.has(element, 'newValue')) {
+      return element.newValue
+        .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${stylish(el.value, depth + 2)}`);
     }
-    const spaceSize = depth * spacesCount;
-    const spaceInd = replacer.repeat(spaceSize);
-    const spaceBr = replacer.repeat(spaceSize - spacesCount);
-    const result = tree.flatMap((element) => {
-      const { name, value, status } = element;
-      const currentValue = (_.has(element, 'children')) ? element.children : value;
-      if (_.has(element, 'newValue')) {
-        return element.newValue
-          .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${iter(el.value, depth + 2)}`);
-      }
-      return `${spaceInd}${symbols[status]} ${name}: ${iter(currentValue, depth + 2)}`;
-    });
-    return ['{', ...result, `${spaceBr}}`].join('\n');
-  };
-  return iter(astTree, 1);
+    return `${spaceInd}${currentSymbol} ${name}: ${stylish(currentValue, depth + 2)}`;
+  });
+  return ['{', ...result, `${spaceBr}}`].join('\n');
 };
 
 export default stylish;
