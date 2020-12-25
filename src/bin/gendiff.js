46ad736b2abb8999c0878ca861756fdff5c766b6
Yura <tt@list.ru>
2020-12-25 17:23:34 +0200

    Added default comand for formater

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,16 +2,18 @@
 import program from 'commander';
 
 import genDiff from '../index.js';
-import stylish from '../print.js';
+// import format from '../formatters/index.js';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows difference.')
-  .option('-f, --format [type]', 'output format', stylish);
+  .option('-f, --format [type]', 'output format', 'stylish');
 
 program
   .arguments('<filepath1> <filepath2>')
   .description('Find difference between two files')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .action(
+    (filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)),
+  );
 
 program.parse(process.argv);
