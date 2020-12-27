2e160413e0752bbe283dd53fd41ab96a5d89cbea
Yura <tt@list.ru>
2020-12-27 20:06:15 +0200

    test for complexticity

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,26 +1,23 @@
 import _ from 'lodash';
 
 const symbols = { removed: '-', added: '+' };
-const stylish = (astTree) => {
-  const iter = (tree, depth) => {
-    const replacer = ' ';
-    const spacesCount = 2;
-    const spaceSize = depth * spacesCount;
-    const spaceInd = replacer.repeat(spaceSize);
-    const spaceBr = replacer.repeat(spaceSize - spacesCount);
-    const result = tree.flatMap((element) => {
-      const { name, value, status } = element;
-      const currentValue = (status === 'nested') ? element.children : value;
-      const currentSymbol = symbols[status] ?? replacer;
-      if (status === 'updated') {
-        return element.newValue
-          .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${iter(el.value, depth + 2)}`);
-      }
-      return `${spaceInd}${currentSymbol} ${name}: ${iter(currentValue, depth + 2)}`;
-    });
-    return ['{', ...result, `${spaceBr}}`].join('\n');
-  };
-  return iter(astTree, 1);
+const stylish = (tree, depth = []) => {
+  const replacer = ' ';
+  const spacesCount = 2;
+  const spaceSize = depth * spacesCount;
+  const spaceInd = replacer.repeat(spaceSize);
+  const spaceBr = replacer.repeat(spaceSize - spacesCount);
+  const result = tree.flatMap((element) => {
+    const { name, value, status } = element;
+    const currentValue = (status === 'nested') ? element.children : value;
+    const currentSymbol = symbols[status] ?? replacer;
+    if (status === 'updated') {
+      return element.newValue
+        .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${stylish(el.value, depth + 2)}`);
+    }
+    return `${spaceInd}${currentSymbol} ${name}: ${stylish(currentValue, depth + 2)}`;
+  });
+  return ['{', ...result, `${spaceBr}}`].join('\n');
 };
 
 export default stylish;
