dee2ebd57b03696172166557a811eb026ed8b098
Yura <tt@list.ru>
2020-12-27 17:55:43 +0200

    plain file modified

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,27 +1,28 @@
 import { normalize as norm } from '../utils.js';
 
-const output = {
-  removed: (p) => `Property '${p}' was removed`,
-  added: (p, val) => `Property '${p}' was added with value: ${norm(val)}`,
-  updated: (p, val, nVal) => `Property '${p}' was updated. From ${norm(val)} to ${norm(nVal)}`,
-  nested: () => [],
-};
-const plain = (elements, propPath = []) => {
-  const result = elements
-    .filter((el) => el.status !== 'unchanged')
-    .flatMap((element) => {
+const plain = (tree) => {
+  const iter = (elements, propPath) => {
+    const result = elements.flatMap((element) => {
       const { name, value, status } = element;
+      const [removed, added] = element.newValue ?? [];
       const propAcc = [...propPath, name];
-      if (status !== 'nested') {
-        if (status === 'updated') {
-          const [removed, added] = element.newValue;
-          return output[status](propAcc.join('.'), removed.value, added.value);
-        }
-        return output[status](propAcc.join('.'), value);
+      const main = `Property '${propAcc.join('.')}' was ${status}`;
+      switch (status) {
+        case 'updated':
+          return `${main}. From ${norm(removed.value)} to ${norm(added.value)}`;
+        case 'added':
+          return `${main} with value: ${norm(value)}`;
+        case 'removed':
+          return main;
+        case 'nested':
+          return iter(element.children, propAcc);
+        default:
+          return [];
       }
-      return plain(element.children, propAcc);
     });
-  return result.join('\n');
+    return result.join('\n');
+  };
+  return iter(tree, []);
 };
 
 export default plain;
