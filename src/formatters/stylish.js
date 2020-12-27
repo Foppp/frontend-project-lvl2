d109a4878d46d8a8c49fc87694616c20a0d70ea2
Yura <tt@list.ru>
2020-12-27 19:44:11 +0200

    formatter stylish file modified

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,25 +1,28 @@
 const symbols = { removed: '-', added: '+' };
-const stylish = (astTree, depth = 1) => {
-  const [treeElement] = astTree;
-  const replacer = ' ';
-  const spacesCount = 2;
-  const spaceSize = depth * spacesCount;
-  const spaceInd = replacer.repeat(spaceSize);
-  const spaceBr = replacer.repeat(spaceSize - spacesCount);
-  if (typeof treeElement !== 'object') {
-    return treeElement;
-  }
-  const result = astTree.flatMap((element) => {
-    const { name, value, status } = element;
-    const currentValue = (status === 'nested') ? element.children : value;
-    const currentSymbol = symbols[status] ?? replacer;
-    if (status === 'updated') {
-      return element.newValue
-        .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${stylish(el.value, depth + 2)}`);
+const stylish = (astTree) => {
+  const iter = (tree, depth) => {
+    const [treeElement] = tree;
+    if (typeof treeElement !== 'object') {
+      return treeElement;
     }
-    return `${spaceInd}${currentSymbol} ${name}: ${stylish(currentValue, depth + 2)}`;
-  });
-  return ['{', ...result, `${spaceBr}}`].join('\n');
+    const replacer = ' ';
+    const spacesCount = 2;
+    const spaceSize = depth * spacesCount;
+    const spaceInd = replacer.repeat(spaceSize);
+    const spaceBr = replacer.repeat(spaceSize - spacesCount);
+    const result = tree.flatMap((element) => {
+      const { name, value, status } = element;
+      const currentValue = (status === 'nested') ? element.children : value;
+      const currentSymbol = symbols[status] ?? replacer;
+      if (status === 'updated') {
+        return element.newValue
+          .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${iter(el.value, depth + 2)}`);
+      }
+      return `${spaceInd}${currentSymbol} ${name}: ${iter(currentValue, depth + 2)}`;
+    });
+    return ['{', ...result, `${spaceBr}}`].join('\n');
+  };
+  return iter(astTree, 1);
 };
 
 export default stylish;
