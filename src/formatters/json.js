2989a38acc3343e0f8f9f71a4a66bc159717f6ab
Yura <tt@list.ru>
2020-12-27 18:51:42 +0200

    formatter json file modified

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,22 +1,18 @@
 import _ from 'lodash';
 
-const formatTree = (tree) => {
-  const iter = (elements, propPath) => {
-    const filterRemoved = elements.filter((el) => el.status !== 'removed');
-    const result = filterRemoved
-      .reduce((acc, element) => {
-        const { name, value, status } = element;
-        const propAcc = [...propPath, name];
-        if (!_.isObject(...value)) {
-          return _.set(acc, propAcc, ...value);
-        }
-        const nestedValue = status === 'nested' ? element.children : value;
-        const updatedValue = status === 'updated' ? element.newValue : nestedValue;
-        return _.merge(acc, iter(updatedValue, propAcc));
-      }, {});
-    return result;
-  };
-  return iter(tree, []);
+const formatTree = (tree, propPath = []) => {
+  const filterRemoved = tree.filter((el) => el.status !== 'removed');
+  const result = filterRemoved.reduce((acc, element) => {
+    const { name, value, status } = element;
+    const propAcc = [...propPath, name];
+    if (!_.isObject(...value)) {
+      return _.set(acc, propAcc, ...value);
+    }
+    const nestedValue = status === 'nested' ? element.children : value;
+    const updatedValue = status === 'updated' ? element.newValue : nestedValue;
+    return _.merge(acc, formatTree(updatedValue, propAcc));
+  }, {});
+  return result;
 };
 
 const formatJson = (tree) => JSON.stringify(formatTree(tree), null, '    ');
