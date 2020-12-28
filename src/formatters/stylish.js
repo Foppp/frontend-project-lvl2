aa387c94ba4b1b91c7539a8e3634183d6ae05813
Yura <tt@list.ru>
2020-12-28 11:00:25 +0200

    test for complexticity-5

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,26 +1,30 @@
 import _ from 'lodash';
 
 const symbols = { removed: '-', added: '+' };
-const stylish = (astTree) => {
-  const iter = (tree, depth) => {
-    const replacer = ' ';
-    const spacesCount = 2;
-    const spaceSize = depth * spacesCount;
-    const spaceInd = replacer.repeat(spaceSize);
-    const spaceBr = replacer.repeat(spaceSize - spacesCount);
-    const result = tree.flatMap((element) => {
-      const { name, value, status } = element;
-      const currentValue = (status === 'nested') ? element.children : value;
-      const currentSymbol = symbols[status] ?? replacer;
-      if (status === 'updated') {
-        return element.newValue
-          .map((el) => `${spaceInd}${symbols[el.status]} ${el.name}: ${iter(el.value, depth + 2)}`);
-      }
-      return `${spaceInd}${currentSymbol} ${name}: ${iter(currentValue, depth + 2)}`;
-    });
-    return ['{', ...result, `${spaceBr}}`].join('\n');
-  };
-  return iter(astTree, 1);
+const stylish = (astTree, depth = 1) => {
+  const [treeElement] = astTree;
+  if (typeof treeElement !== 'object') {
+    return treeElement;
+  }
+  const replacer = ' ';
+  const spacesCount = 2;
+  const spaceSize = depth * spacesCount;
+  const spaceInd = replacer.repeat(spaceSize);
+  const spaceBr = replacer.repeat(spaceSize - spacesCount);
+  const result = astTree.flatMap((element) => {
+    const { name, value, status } = element;
+    const currentValue = (status === 'nested') ? element.children : value;
+    const currentSymbol = symbols[status] ?? replacer;
+    if (status === 'updated') {
+      const [removed, added] = element.newValue;
+      return [
+        `${spaceInd}${symbols[removed.status]} ${removed.name}: ${stylish(removed.value, depth + 2)}`,
+        `${spaceInd}${symbols[added.status]} ${added.name}: ${stylish(added.value, depth + 2)}`,
+      ];
+    }
+    return `${spaceInd}${currentSymbol} ${name}: ${stylish(currentValue, depth + 2)}`;
+  });
+  return ['{', ...result, `${spaceBr}}`].join('\n');
 };
 
 export default stylish;
