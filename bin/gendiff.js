bf9409cb787fd7e9f47e9b2000d356826b3cbc6e
Yura <tt@list.ru>
2020-12-30 15:28:16 +0200

    removed unnessesary imports

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,16 +2,12 @@
 import program from 'commander';
 
 import genDiff from '../index.js';
-// import format from '../formatters/index.js';
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows difference.')
-  .option('-f, --format [type]', 'output format', 'stylish');
-
-program
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .description('Find difference between two files')
+  .description('Compares two configuration files and shows difference.')
   .action(
     (filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)),
   );
