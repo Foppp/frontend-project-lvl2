22c20107d97aca51583f675d4a948db267bbd82f
Yura <tt@list.ru>
2021-01-06 13:10:49 +0200

    modified error catch

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -7,6 +7,12 @@ const formatters = {
   json: (tree) => JSON.stringify(tree),
 };
 
-const format = (tree, formatterName) => formatters[formatterName](tree);
-
+const format = (tree, formatterName) => {
+  try {
+    return formatters[formatterName](tree);
+  } catch (e) {
+    throw new Error(`There is no such file from this path or wrong name of formatter.
+    ${e}`);
+  }
+};
 export default format;
