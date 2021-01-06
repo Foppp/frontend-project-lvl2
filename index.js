42bdc4a518831aebaaa59c55714aa8a9c1db9337
Yura <tt@list.ru>
2021-01-06 12:52:16 +0200

    removed unnessesary imports

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import parseFile from './src/parser.js';
-import makeDiff from './src/diff.js';
-import format from './src/formatters/index.js';
+import genDiff from './src/index.js';
 
-const genDiff = (path1, path2, formatName = 'stylish') => {
-  const parsedFile1 = parseFile(path1);
-  const parsedFile2 = parseFile(path2);
-  const result = makeDiff(parsedFile1, parsedFile2);
-  return format(result, formatName);
-};
 export default genDiff;
