e1f75af64b8885661505d575fd189d83fd59296c
Yura <tt@list.ru>
2021-01-12 20:57:22 +0200

    changed logic in test, test.each

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,20 +9,20 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const jsonFile1 = getFixturePath('nestedJsonFile1.json');
-const jsonFile2 = getFixturePath('nestedJsonFile2.json');
-const yamlFile1 = getFixturePath('yamlFileA.yml');
-const yamlFile2 = getFixturePath('yamlFileB.yml');
+const formats = ['json', 'yml'];
+const expectedStylish = readFile('stylishResult.txt');
+const expectedPlain = readFile('plainResult.txt');
+const expectedJson = readFile('jsonResult.json');
 
-const stylishResult = readFile('stylishResult.txt');
-const plainResult = readFile('plainResult.txt');
-const jsonResult = readFile('jsonResult.json');
-
-const fileFormats = [['json', jsonFile1, jsonFile2], ['yaml', yamlFile1, yamlFile2]];
-const resultFormats = [['stylish', stylishResult], ['plain', plainResult], ['json', jsonResult]];
-
-describe.each(fileFormats)('Compare two .%s files', (fileFormat, file1, file2) => {
-  test.each(resultFormats)(`.${fileFormat} file in %s formatter`, (formatter, expected) => {
-    expect(genDiff(file1, file2, formatter)).toEqual(expected);
+describe('Compare two files', () => {
+  test.each(formats)('%s files', (fileFormat) => {
+    const file1 = getFixturePath(`file1.${fileFormat}`);
+    const file2 = getFixturePath(`file2.${fileFormat}`);
+    const actualStylish = genDiff(file1, file2, 'stylish');
+    const actualPlain = genDiff(file1, file2, 'plain');
+    const actualJson = genDiff(file1, file2, 'json');
+    expect(actualStylish).toEqual(expectedStylish);
+    expect(actualPlain).toEqual(expectedPlain);
+    expect(actualJson).toEqual(expectedJson);
   });
 });
