bbf6f5c83a0d6fbce03a86d8cf5a4694871ed2db
Yura <tt@list.ru>
2021-01-17 15:29:22 +0200

    syntax refactoring names

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
 
-const makeIdent = (depth) => '  '.repeat(depth);
+const makeIndent = (depth) => '  '.repeat(depth);
 
 const stringifyValue = (obj, depth) => {
   if (!_.isPlainObject(obj)) {
     return obj;
   }
   const stringifiedElements = Object.entries(obj)
-    .map(([key, val]) => `${makeIdent(depth)}  ${key}: ${stringifyValue(val, depth + 2)}`);
-  return `{\n${stringifiedElements.join('\n')}\n${makeIdent(depth - 1)}}`;
+    .map(([key, val]) => `${makeIndent(depth)}  ${key}: ${stringifyValue(val, depth + 2)}`);
+  return `{\n${stringifiedElements.join('\n')}\n${makeIndent(depth - 1)}}`;
 };
 
 const mapping = {
-  unchanged: (element, depth) => `${makeIdent(depth)}  ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-  removed: (element, depth) => `${makeIdent(depth)}- ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-  added: (element, depth) => `${makeIdent(depth)}+ ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-  nested: (element, depth, iter) => `${makeIdent(depth)}  ${element.name}: ${iter(element.children, depth + 2)}`,
+  unchanged: (element, depth) => `${makeIndent(depth)}  ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
+  removed: (element, depth) => `${makeIndent(depth)}- ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
+  added: (element, depth) => `${makeIndent(depth)}+ ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
+  nested: (element, depth, iter) => `${makeIndent(depth)}  ${element.name}: ${iter(element.children, depth + 2)}`,
   updated: (element, depth) => [
-    `${makeIdent(depth)}- ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-    `${makeIdent(depth)}+ ${element.name}: ${stringifyValue(element.newValue, depth + 2)}`,
+    `${makeIndent(depth)}- ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
+    `${makeIndent(depth)}+ ${element.name}: ${stringifyValue(element.newValue, depth + 2)}`,
   ],
 };
 
@@ -26,7 +26,7 @@ const stylish = (tree) => {
   const iter = (astTree, depth) => {
     const stringifiedElements = astTree
       .flatMap((astElement) => mapping[astElement.status](astElement, depth, iter));
-    return `{\n${stringifiedElements.join('\n')}\n${makeIdent(depth - 1)}}`;
+    return `{\n${stringifiedElements.join('\n')}\n${makeIndent(depth - 1)}}`;
   };
   return iter(tree, 1);
 };
