cc8fe4b67e3113cb6ea80b8b09f3ddfb5798d205
Yura <tt@list.ru>
2021-01-18 14:15:53 +0200

    minor refactoring

--- src/diff.js
+++ src/diff.js
@@ -4,17 +4,17 @@ const makeDiff = (data1, data2) => {
   const uniqKeys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(uniqKeys);
   return sortedKeys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      const children = makeDiff(data1[key], data2[key]);
-      return {
-        name: key, value: data1[key], status: 'nested', children,
-      };
-    }
     if (!_.has(data2, key)) {
       return { name: key, value: data1[key], status: 'removed' };
     }
     if (!_.has(data1, key)) {
       return { name: key, value: data2[key], status: 'added' };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      const children = makeDiff(data1[key], data2[key]);
+      return {
+        name: key, value: data1[key], status: 'nested', children,
+      };
     }
     if (!_.isEqual(data1[key], data2[key])) {
       return {
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
 
-const makeIndent = (depth) => '  '.repeat(depth);
+const makeIndent = (depth) => '  '.repeat(depth * 2);
 
 const stringifyValue = (obj, depth) => {
   if (!_.isPlainObject(obj)) {
     return obj;
   }
   const stringifiedElements = Object.entries(obj)
-    .map(([key, val]) => `${makeIndent(depth)}  ${key}: ${stringifyValue(val, depth + 2)}`);
-  return `{\n${stringifiedElements.join('\n')}\n${makeIndent(depth - 1)}}`;
+    .map(([key, val]) => `${makeIndent(depth + 1)}    ${key}: ${stringifyValue(val, depth + 1)}`);
+  return `{\n${stringifiedElements.join('\n')}\n${makeIndent(depth + 1)}}`;
 };
 
 const mapping = {
-  unchanged: (element, depth) => `${makeIndent(depth)}  ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-  removed: (element, depth) => `${makeIndent(depth)}- ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-  added: (element, depth) => `${makeIndent(depth)}+ ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-  nested: (element, depth, iter) => `${makeIndent(depth)}  ${element.name}: ${iter(element.children, depth + 2)}`,
+  unchanged: (element, depth) => `${makeIndent(depth)}    ${element.name}: ${stringifyValue(element.value, depth)}`,
+  removed: (element, depth) => `${makeIndent(depth)}  - ${element.name}: ${stringifyValue(element.value, depth)}`,
+  added: (element, depth) => `${makeIndent(depth)}  + ${element.name}: ${stringifyValue(element.value, depth)}`,
+  nested: (element, depth, iter) => `${makeIndent(depth)}    ${element.name}: ${iter(element.children, depth + 1)}`,
   updated: (element, depth) => [
-    `${makeIndent(depth)}- ${element.name}: ${stringifyValue(element.value, depth + 2)}`,
-    `${makeIndent(depth)}+ ${element.name}: ${stringifyValue(element.newValue, depth + 2)}`,
+    `${makeIndent(depth)}  - ${element.name}: ${stringifyValue(element.value, depth)}`,
+    `${makeIndent(depth)}  + ${element.name}: ${stringifyValue(element.newValue, depth)}`,
   ],
 };
 
@@ -26,9 +26,9 @@ const stylish = (tree) => {
   const iter = (astTree, depth) => {
     const stringifiedElements = astTree
       .flatMap((astElement) => mapping[astElement.status](astElement, depth, iter));
-    return `{\n${stringifiedElements.join('\n')}\n${makeIndent(depth - 1)}}`;
+    return `{\n${stringifiedElements.join('\n')}\n${makeIndent(depth)}}`;
   };
-  return iter(tree, 1);
+  return iter(tree, 0);
 };
 
 export default stylish;
